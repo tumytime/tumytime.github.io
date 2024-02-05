28dee0aeee8564df5349505eaefa22fcfdda7aa7
tumytime <2259763426@qq.com>
2024-02-05 17:10:00 +0800

    Site updated: 2024-02-05 17:10:00

--- live2d/index.js
+++ live2d/index.js
@@ -1,18 +1,18 @@
 L2Dwidget.init({
   model: {
     jsonPath:
-      "https://unpkg.com/live2d-widget-model-unitychan@1.0.5/assets/unitychan.model.json",
+      "https://unpkg.com/live2d-widget-model-chitose@1.0.5/assets/unitychan.model.json",
     scale: 1,
   },
   display: {
-    position: "left", //模型的表现位置
+    position: "right", //模型的表现位置
     width: 150, //模型的宽度
     height: 300, //模型的高度
     hOffset: -50, // 向左向右偏移
     vOffset: -150, // 向上向下偏移, 根据情况可以自己调整数值
   },
   mobile: {
-    show: false, // 手机端不显示
+    show: true, // 手机端显示
     scale: 0.5,
   },
   react: {
